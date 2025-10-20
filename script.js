01db0a76462f9d9fc5e4de928e5cefce1e79cafc
skateloterie <gamingakla1@gmail.com>
2025-10-20 15:06:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById("emailForm").addEventListener("submit", function(event) {
+document.getElementById("emailForm")?.addEventListener("submit", function(event) {
   event.preventDefault();
   const email = document.getElementById("email").value;
   const message = document.getElementById("message");
-  message.textContent = `Thanks for subscribing, ${email}!`;
+  message.textContent = `Děkujeme za přihlášku, ${email}!`;
   document.getElementById("emailForm").reset();
 });
